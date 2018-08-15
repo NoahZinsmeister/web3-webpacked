cb52f677871aeba210bec861980b8f90c0227267
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-08-15 10:49:12 -0400

    remove spread operator for compatibility

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 const listeners = require('./web3Listeners')
 const utilities = require('./web3Utilities')
 
-module.exports = {
-  ...utilities,
-  ...listeners
-}
+module.exports = Object.assign({}, utilities, listeners)
